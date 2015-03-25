225aa30d86136fbff9e537fb9fa64659a8f57276
Mirinth <mirinth@gmail.com>
2015-03-25 10:31:16 -0600

    Added BusinessRules class

---
+++ SELibrary/SELibrary/BusinessRules.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SELibrary
+{
+    /// <summary>
+    /// Enumerates the various error conditions the BusinessRules
+    /// object can encounter while servicing requests.
+    /// </summary>
+    enum ErrorMessage
+    {
+        // none so far
+    }
+
+    static class BusinessRules
+    {
+        /// <summary>
+        /// The event raised when the date changes.
+        /// </summary>
+        public static event Action<DateTime> DateChanged;
+
+        /// <summary>
+        /// The event raised when the BusinessRules encounteres
+        /// an error while trying to service a request.
+        /// </summary>
+        public static event Action<ErrorMessage> ErrorEncountered;
+
+        /// <summary>
+        /// Gets the current date, according to the BusinessRules
+        /// </summary>
+        public static DateTime CurrentDate { get; private set; }
+
+        /// <summary>
+        /// Initializes the BusinessRules object.
+        /// </summary>
+        static BusinessRules()
+        {
+            CurrentDate = new DateTime(2015, 1, 1);
+
+            // Events need to be initialized, but you can't have an
+            // Action<T> that doesn't point to a method, so point them
+            // to a method that does nothing since the BusinessRules
+            // doesn't really care when the events occur right now.
+            DateChanged = new Action<DateTime>((x) => { return; });
+            ErrorEncountered = new Action<ErrorMessage>((x) => { return; });
+        }
+
+        /// <summary>
+        /// Checks out the given item to the given patron.
+        /// </summary>
+        /// <param name="item">The item to check out.</param>
+        /// <param name="toPatron">The patron to loan it to.</param>
+        public static void CheckOut(Media item, Patron toPatron)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Checks in the given item.
+        /// </summary>
+        /// <param name="item">The item to check in.</param>
+        public static void CheckIn(Media item)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets a list of all patrons.
+        /// </summary>
+        /// <returns>A list of all patrons.</returns>
+        public static List<Patron> ListPatrons()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets a list of all media.
+        /// </summary>
+        /// <returns>A list of all media.</returns>
+        public static List<Media> ListMedia()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets a list of all overdue media.
+        /// </summary>
+        /// <returns>A list of all overdue media.</returns>
+        public static List<Media> ListOverdueMedia()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets a list of all media items checked out to a patron.
+        /// </summary>
+        /// <param name="byPatron">The patron of interest.</param>
+        /// <returns>A list of all media items checked out to the patron.</returns>
+        public static List<Media> ListMediaByPatron(Patron byPatron)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Simulates the passage of time
+        /// </summary>
+        public static void PassTime()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
