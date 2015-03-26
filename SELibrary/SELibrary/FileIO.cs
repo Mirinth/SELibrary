4ff166a1358d51ff654cc7497508d5af1a99c642
Mirinth <mirinth@gmail.com>
2015-03-25 20:25:17 -0600

    Added FileIO and ListInfo files
    FileIO and ListInfo were only present in the zip file, which made them
    inaccessible to the rest of the project.

---
+++ SELibrary/SELibrary/FileIO.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CS2450_Library_Software
+{
+    class FileIO
+    {
+        private FileStream _fileStream;
+        private SaveFileDialog saveDlg;
+        private OpenFileDialog openDlg;
+        private string filePath;
+
+        public FileIO()
+        {
+            _fileStream = null;
+            filePath = null;
+        }
+
+        internal Patron Patron
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        internal Media Media
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        //Opens The Save Stream
+        public void SaveStream()
+        {
+
+        }
+
+        //Opens The Open Stream
+        public void OpenStream()
+        {
+
+        }
+
+        //Reads in a patron Database
+        public List<Patron> ReadPatrons()
+        {
+            return null;
+        }
+
+        //Writes out a patron Database
+        public void WritePatrons(List<Patron> allPatrons)
+        {
+
+        }
+
+        //Reads in a media database
+        public List<Media> ReadMedia()
+        {
+            return null;
+        }
+
+        //Writes out a media database
+        public void WriteMedia(List<Media> allMedia)
+        {
+
+        }
+
+        //Closes the Steam
+        public void CloseStream()
+        {
+
+        }
+    }
+}
---
+++ SELibrary/SELibrary/ListInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS2450_Library_Software
+{
+    class ListInfo
+    {
+        public ListInfo()
+        {
+
+        }
+
+        //Lists All Media
+        public void ListAllMedia(List<Media> allBooks)
+        {
+
+        }
+
+        //Lists A Single Person Books
+        public void ListPatronsBooks(Patron person)
+        {
+
+        }
+
+        //Lists All Overdue Books
+        public void ListAllOverdueBooks(List<Media> allBooks)
+        {
+
+        }
+    }
+}
