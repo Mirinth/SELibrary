e90cd4d7ca05541362abe818a9f180261df1606e
Mirinth <mirinth@gmail.com>
2015-04-06 16:03:44 -0600

    Created data generator with random name generator

---
+++ SELibrary/SELibrary/Integration/DataGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace SELibrary.Integration
+{
+    static class DataGenerator
+    {
+        private static string[] FirstNames =
+        {
+            "John",
+            "Jack",
+            "Joe",
+            "John",
+            "Jim",
+            "Jane",
+            "Jill",
+            "Jan",
+            "Jen",
+            "June"
+        };
+
+        private static string[] LastNames =
+        {
+            "Smith",
+            "Doe",
+            "Fisher",
+            "Baker",
+            "House",
+            "Townsend",
+            "Castle",
+            "Cheswick",
+            "Jameson",
+            "Lastname"
+        };
+
+        public static string RandomName()
+        {
+            Random rng = new Random();
+            string first = FirstNames[rng.Next(FirstNames.Length)];
+            string last = LastNames[rng.Next(LastNames.Length)];
+
+            return first + " " + last;
+        }
+    }
+}
